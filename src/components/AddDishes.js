d3f0dae12ec80ad2fbadf5b20206fe681358104e
Harsh Patel <harshpatel235027@gmail.com>
2021-04-23 18:08:23 -0400

    Final

--- src/components/AddDishes.js
+++ src/components/AddDishes.js
@@ -12,19 +12,16 @@ const AddDishes = () => {
  
 
     const submit = (e) => {
-        // fetch('https://dishes-backend.herokuapp.com/api/menu/add', {
-        //   method: 'POST',
-        //   body: JSON.stringify(e),
-        //   headers: { 'Content-Type': 'application/json' },
-        // })
-        // .then(response => { 
-        //         setSubmitted(true) 
-        //         response.json()})
-        // .then(data => setMenu(data))
-        console.log(JSON.stringify(e));
-        axios.post("http://localhost:5000/upload",e)
-        .then(response=>{console.log(response);})
-      }
+        fetch('https://dishes-backend.herokuapp.com/api/menu/add', {
+          method: 'POST',
+          body: JSON.stringify(e),
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(response => { 
+                setSubmitted(true) 
+                response.json()})
+        .then(data => setMenu(data))
+        }
 
       
 
@@ -45,7 +42,7 @@ const AddDishes = () => {
                 
                     <h4>Add Dishes into Menu</h4>
                     <form onSubmit={handleSubmit(submit)}>
-                    {/* <div className="form-group d-flex ms-5">
+                    <div className="form-group d-flex ms-5">
                         <input 
                             type="text" 
                             className="form-control" 
@@ -54,7 +51,7 @@ const AddDishes = () => {
                             <div>{errors.itemname && "Dish Name is required"}</div>
                         
                         
-                    </div> */}
+                    </div>
                     <div class="form-group d-flex ms-5">
                         <input 
                             type="file" 
@@ -64,15 +61,15 @@ const AddDishes = () => {
                             <div>{errors.lastname && "Select File to Upload"}</div>
                         
                     </div>
-                    {/* <div class="form-group d-flex ms-5">
+                    <div class="form-group d-flex ms-5">
                             <select  className="form-control"  {...register("category", { required: "select one option"})}>
                                 <option value="">--Select Category--</option>
                                 <option value="Veg">Veg</option>
                                 <option value="Non-Veg">Non-Veg</option>
                             </select>
                             <div>{errors.category && "Category is required"}</div>					
-                    </div> */}
-                    {/* <div class="form-group d-flex ms-5">
+                    </div>
+                    <div class="form-group d-flex ms-5">
                             <select  className="form-control"  {...register("location", { required: "select one option"})}>
                                 <option value="">--Select Location--</option>
                                 <option value="Etobicoke">Etobicoke</option>
@@ -80,23 +77,23 @@ const AddDishes = () => {
                                 <option value="Downtown">Downtown</option>
                             </select>
                             <div>{errors.location && "Location is required"}</div>					
-                    </div> */}
-                    {/* <div class="form-group d-flex ms-5">
+                    </div>
+                    <div class="form-group d-flex ms-5">
                         <input 
                             type="text" 
                             className="form-control" 
                             placeholder="Enter Price" 
                             {...register("price", { required: true})}/>
                             <div>{errors.price && "price is required"}</div>					
-                    </div> */}
-                    {/* <div class="form-group d-flex ms-5">
+                    </div>
+                    <div class="form-group d-flex ms-5">
                         <input 
                             type="text" 
                             className="form-control" 
                             placeholder="Enter Your Email" 
                             {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}/>
                             <div>{errors.email && "Email is required"}</div>					
-                    </div> */}
+                    </div>
                     <div className="form-group d-flex ms-5 justify-content-evenly">
                         <button type="submit" className="btn btn-primary ">Add Dish</button>
                     </div>
