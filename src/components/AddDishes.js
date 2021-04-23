c7ad538d20633b24fa2b7b33cc0abfca3fcc7dfc
Harsh Patel <harshpatel235027@gmail.com>
2021-04-23 19:23:24 -0400

    Final Commit

--- src/components/AddDishes.js
+++ src/components/AddDishes.js
@@ -1,21 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useForm} from 'react-hook-form';
 import {Redirect} from 'react-router'
-import axios from 'axios';
+import jwt_decode from "jwt-decode";
 import '../css/Registration.css'
 
 const AddDishes = () => {
     const {register, formState: { errors }, handleSubmit} = useForm();
+    const [user, setUser] = useState([]);
+    const [token, setToken] = useState("");
+    const [loading,setLoading]=useState(true);
     const [menu, setMenu] = useState([]);
     const [submitted, setSubmitted] = useState(false);
 
+    useEffect(()=>
+    {
+        var token = localStorage.getItem('token');
+        console.log(token);
+        if(token==="")
+        {
+            setLoading(false);
+        }
+        else
+        {
+            setToken(token);
+            var decoded = jwt_decode(token);
+            setUser(decoded.user);
+        }
+    }, []);
  
 
     const submit = (e) => {
-        fetch('https://dishes-backend.herokuapp.com/api/menu/add', {
+
+        fetch('http://localhost:4000/api/menu/add', {
           method: 'POST',
-          body: JSON.stringify(e),
-          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+              itemname:e.itemname,
+              image:"abc.jpg",
+              location:e.location,
+              category:e.category,
+              price:e.price
+          }),
+          headers: {'Content-Type': 'application/json', 
+          'x-access-token':token},
         })
         .then(response => { 
                 setSubmitted(true) 
@@ -25,11 +51,10 @@ const AddDishes = () => {
 
       
 
-      console.log(menu);
 
       if (submitted) {
         return <Redirect push to={{
-          pathname: '/',
+          pathname: '/myDishes',
         }}
         />
       }  
@@ -46,6 +71,7 @@ const AddDishes = () => {
                         <input 
                             type="text" 
                             className="form-control" 
+                            name="itemname"
                             placeholder="Enter Dish Name" 
                             {...register("itemname", { required: true})}/>
                             <div>{errors.itemname && "Dish Name is required"}</div>
@@ -56,13 +82,14 @@ const AddDishes = () => {
                         <input 
                             type="file" 
                             className="form-control" 
+                            name="image"
                             placeholder="Upload Image"
                             {...register("image", { required: true})}/>
                             <div>{errors.lastname && "Select File to Upload"}</div>
                         
                     </div>
                     <div class="form-group d-flex ms-5">
-                            <select  className="form-control"  {...register("category", { required: "select one option"})}>
+                            <select  className="form-control" name="category"  {...register("category", { required: "select one option"})}>
                                 <option value="">--Select Category--</option>
                                 <option value="Veg">Veg</option>
                                 <option value="Non-Veg">Non-Veg</option>
@@ -70,7 +97,7 @@ const AddDishes = () => {
                             <div>{errors.category && "Category is required"}</div>					
                     </div>
                     <div class="form-group d-flex ms-5">
-                            <select  className="form-control"  {...register("location", { required: "select one option"})}>
+                            <select  className="form-control" name="location"  {...register("location", { required: "select one option"})}>
                                 <option value="">--Select Location--</option>
                                 <option value="Etobicoke">Etobicoke</option>
                                 <option value="North York">North York</option>
@@ -83,6 +110,7 @@ const AddDishes = () => {
                             type="text" 
                             className="form-control" 
                             placeholder="Enter Price" 
+                            name="price"
                             {...register("price", { required: true})}/>
                             <div>{errors.price && "price is required"}</div>					
                     </div>
@@ -91,6 +119,7 @@ const AddDishes = () => {
                             type="text" 
                             className="form-control" 
                             placeholder="Enter Your Email" 
+                            name="email"
                             {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}/>
                             <div>{errors.email && "Email is required"}</div>					
                     </div>
