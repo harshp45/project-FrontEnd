3bf6ecb6b284f4db3d940062050b304104bf3ab9
Harsh Patel <harshpatel235027@gmail.com>
2021-04-23 20:53:45 -0400

    Finish!!!:)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,9 +29,11 @@ function Footer() {
             
             <div className="row">
                 <div className="d-flex flex-row">
-                <span style={{color: "whitesmoke"}} className="mx-3"><i class="fas fa-envelope"></i>&nbsp;contact.homestyledelicacies@gmail.com</span>
-                <span style={{color: "whitesmoke"}} className="mx-3"><i class="fas fa-phone"></i>&nbsp;+1 647-571-3096</span>
-                <span style={{color: "whitesmoke"}} className="mx-3"><i class="fas fa-map-marker"></i>&nbsp;contact.homestyledelicacies@gmail.com</span>
+                <a href="mailto:contact.homestyledelicacies@gmail.com" className="linkDecor"><span style={{color: "whitesmoke"}} className="mx-3"><i className="fas fa-envelope"></i>&nbsp;contact.homestyledelicacies@gmail.com</span></a>
+                <a href="tel:16475713096" className="linkDecor"><span style={{color: "whitesmoke"}} className="mx-3"><i className="fas fa-phone"></i>&nbsp;+1 647-571-3096</span></a>
+                <a href="https://www.google.com/maps/place/1294+Islington+Ave,+Etobicoke,+ON+M9A+3K2/@43.648946,-79.5284145,17z/data=!3m1!4b1!4m5!3m4!1s0x882b37a8a5b7f145:0x6b50dfe593cf8995!8m2!3d43.648946!4d-79.5262258" className="linkDecor">
+                <span style={{color: "whitesmoke"}} className="mx-3"><i className="fas fa-map-marker"></i>&nbsp;1294 Islington Avenue, Etobicoke, M9A3K2.</span>
+                    </a>
                 </div>
             </div>
             <div className="row">
