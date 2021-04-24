47493c47b99fa310cf337e6479037ef6bb446910
Harsh Patel <harshpatel235027@gmail.com>
2021-04-23 21:44:59 -0400

    Final changes done

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -18,7 +18,7 @@ function userHeader() {
 
             <div className="container-fluid">
                 
-            <Link className="navbar-brand logo" href="/"><img className="hd-logo" src="/Logo.png" />Homestyle Delicacies</Link>
+            <Link className="navbar-brand logo" to="/userdashboard"><img className="hd-logo" src="/Logo.png" />Homestyle Delicacies</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
